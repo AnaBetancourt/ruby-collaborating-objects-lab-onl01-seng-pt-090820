d62e7213756d5ccb595d2deb06e75d43005fc6f9
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-06 23:17:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,15 +23,15 @@ class Artist
   end 
 
   def self.find(name)
-      self.all.find{|artist| artist.name == name}
+    self.all.find{|artist| artist.name == name}
   end
 
   def self.find_or_create_by_name(name)
-      if self.find(name)
-        self.find(name)
-      else 
-        self.new(name)
-      end
+    if self.find(name)
+      self.find(name)
+    else 
+      self.new(name)
+    end
   end
   
   def print_songs
