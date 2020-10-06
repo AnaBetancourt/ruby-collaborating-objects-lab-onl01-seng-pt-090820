d5a3d03099594d1aadee99683d7b7a68d7bc62f0
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-06 22:01:29 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,50 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def add_song(song)
+    song.artist = self
+  end 
+  
+  def songs 
+    Song.all.select{|song| song.artist == self}
+  end 
+
+# def self.find(name)
+#     Artist.all.find{|artist| artist.name == name}
+#   end
+
+# def self.find_or_create_by_name(name)
+#     if self.find(name)
+#       return self.find(name)
+#     else
+#       return Artist.new(name)
+#     end
+#   end
+
+  def self.find_or_create_by_name(name)
+    self.all.find do |artist| 
+    if artist.name == name
+       artist
+     else 
+       new = self.new(name)
+     end
+    end
+  end
+  
+  def print_songs
+    puts self.songs
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Song 
+  attr_accessor :name, :artist 
+
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.new_by_filename(filename)
+    name = filename.split(" - ")[1]
+    song = self.new(name)
+    song.artist = filename.split(" - ")[0]
+    song
+  end
+
+  def self.artist_name=(name)
+    new_artist = Artist.find_or_create_by_name(name)
+    self.artist = new_artist
+    Artist.add_song(self)
+  end
+  
+  
+end
