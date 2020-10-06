1b297fd140efa6ce21e19da4702f1091acb0361a
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-06 23:34:30 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,11 +11,6 @@ class MP3Importer
     end
   end
   
-  def files
-    Dir.glob("#{path}/*.mp3").collect do |filepath|
-      filepath.gsub("./spec/fixtures/mp3s/", "")
-  end
-  
   # def import 
   #   self.files.each do |filename|
   #     Song.new_by_filename(filename)
