e776e79ab7c93816b3a8ebc75773a36152564f57
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-07 00:04:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
   def self.new_by_filename(filename)
     song_name = filename.split(" - ")[1]
     song = self.new(song_name)
-    Artist.new()
+    Artist.new(filename.split(" - ")[0])
     song.artist =
     binding.pry
     song
